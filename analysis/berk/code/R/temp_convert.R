ef513be936a822b4527fa145fd341107c2d940ce
Berk Norman <thenapster@comcast.net>
2015-05-27 14:40:09 -0400

    new temp

--- analysis/berk/code/R/temp_convert.R
+++ analysis/berk/code/R/temp_convert.R
@@ -1,18 +1,5 @@
-#library(testthat)
-
 f_to_k <- function(temp) {
 	convert_temp <- ((temp-32)*5/9)+273.15
 	return(convert_temp)	
 }
-#expect_equal(f_to_k(32)==273.15)
-
-k_to_c <- function(temp) {
-	return(temp+273.15) }
-
-k_to_c(0)
-
-f_to_c<-function(temp) {
-	return(k_to_c(f_to_k(temp)))
-}
-
-f_to_c(32)
+f_to_k(32)
